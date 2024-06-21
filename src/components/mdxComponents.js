5dbdaa4c310d1cc0e4315dafe62cec072d143f21
suejinkim20 <kim.suejin.20@gmail.com>
2024-06-21 12:35:59 -0400

    configure mdx component configuration

---
+++ src/components/mdxComponents.js
@@ -0,0 +1,13 @@
+import React, { useMemo } from 'react'
+import { MDXProvider } from "@mdx-js/react"
+
+export const Markdown = ({ children }) => {
+  const componentMap = useMemo(() => ({
+    p: function Anchor({ node, href, children, ...props }) { return <p style={{lineHeight: '1.5', letterSpacing: '0.2px' }}  { ...props }>{children}</p> },
+    li: function Anchor({ node, href, children, ...props }) { return <li style={{lineHeight: '1.5', letterSpacing: '0.2px' }}>{children}</li> },
+  }), [])
+
+  return (
+    <MDXProvider components={ componentMap } children={ children } />
+  )
+}
