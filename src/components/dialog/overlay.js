abb2ffd35087638a49282907c941b247530cc262
suejinkim20 <kim.suejin.20@gmail.com>
2024-07-19 13:49:51 -0400

    extract dialog UI components from dialog-context file and place in components directory

---
+++ src/components/dialog/overlay.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Overlay = styled.div`
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: #00000099;
+  z-index: 999;
+`;
