11d77acdfdeac1c46de5062e113b5d104339ee26
Matt Watson <mwatson@renci.org>
2024-07-28 15:17:56 -0400

    fix ssr page wrapper

--- gatsby-ssr.jsx
+++ gatsby-ssr.jsx
@@ -1,8 +1,15 @@
-const React = require("react")
-const Layout = require("./src/components/layout").default
+import React from "react";
+import { Layout } from "./src/components/layout";
+import { DialogProvider } from "./src/contexts/dialog-context";
+import "./src/styles/normalize.css";
+import "./src/styles/customize.css";
 
-exports.wrapPageElement = ({ element, props }) => {
+export const wrapPageElement = ({ element, props }) => {
   // props provide same data to Layout as Page element will get
   // including location, data, etc - you don't need to pass it
-  return <Layout {...props}>header<br/>{element}</Layout>
+  return (
+    <DialogProvider>
+      <Layout>{element}</Layout>
+    </DialogProvider>
+)
 }
