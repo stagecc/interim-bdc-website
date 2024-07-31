814d8c7affd84f8662a4686a48c86c71a788082b
Matt Watson <mwatson@renci.org>
2024-07-31 09:12:43 -0400

    add floating box margin

--- src/components/layout/floating-content.js
+++ src/components/layout/floating-content.js
@@ -6,8 +6,10 @@ export const FloatingContentWrapper = styled.div(({placement, type})=>`
   background-color: ${ type==="text" && "rgba(237, 240, 244, 0.8)"};
   box-shadow: ${ type==="text" && "4px 4px 12px rgba(186, 194, 204, 0.5)"};
   padding: 0 1.5rem;
-  margin-left: ${ placement==="right" && "1.5rem"};
-  margin-right: ${ placement==="left" && "1.5rem"};
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: ${ placement === "right" ? "1.5rem" : 0 };
+  margin-right: ${ placement === "left" ? "1.5rem" : 0 };
   @media screen and (max-width: 768px) {
     width: 100%;
     float: none;
