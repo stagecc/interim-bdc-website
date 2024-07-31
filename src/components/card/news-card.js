8d0f95676b97475aeaed738c60fecd93b4399877
Matt Watson <mwatson@renci.org>
2024-07-31 11:07:25 -0400

    tidy shadows & anims on news cov cards

--- src/components/card/news-card.js
+++ src/components/card/news-card.js
@@ -13,20 +13,22 @@ const NewsCardContainer = styled(Card)`
   overflow: hidden;
   height: 100%;
   background: linear-gradient(180deg, var(--color-blueberry) 0%, #314f6e 100%);
-  transition: transform 700ms;
-  filter: drop-shadow(6px 6px 4px var(--color-lightgrey));
-  transition: filter 100ms ease-in;
-  &:hover, &:focus {
-    transform: translateY(-1.5px);
-    filter: drop-shadow(10px 10px 6px var(--color-lightgrey));
-  };
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   padding: 1rem;
+  filter: 
+    drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.05))
+    drop-shadow(4px -4px 4px rgba(0, 0, 0, 0.05))
+    drop-shadow(-4px 8px 4px rgba(0, 0, 0, 0.05));
+  &:hover, &:focus {
+  filter: 
+    drop-shadow(4px 4px 6px rgba(0, 0, 0, 0.1))
+    drop-shadow(6px -6px 6px rgba(0, 0, 0, 0.1))
+    drop-shadow(-6px 8px 6px rgba(0, 0, 0, 0.1));
+  };
+  transition: filter 100ms ease-in;
 `
-
-//add a filter on it to change the color
 
 const NewsCardHeading = styled.h2`
   line-height: 1.5; 
@@ -40,8 +42,7 @@ const NewsCardHeading = styled.h2`
   };
   display: flex;
   justify-content: flex-start;
-`
-
+`;
 
 export const NewsCard = ({ newsItem }) => {
   const {
@@ -50,7 +51,8 @@ export const NewsCard = ({ newsItem }) => {
     newsDate, 
     newsSource, 
     external, 
-    loginRequired } = newsItem
+    loginRequired,
+  } = newsItem;
 
   return (
     <Link to={newsLink} noIcon>
