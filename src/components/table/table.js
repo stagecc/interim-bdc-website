368ade814a8922a4d793934c38d6d4b25e418789
suejinkim20 <kim.suejin.20@gmail.com>
2024-07-31 07:46:52 -0400

    add table styles

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -3,9 +3,16 @@ import styled from "styled-components";
 export const Table = styled.table`
   width: 100%;
   border-collapse: collapse;
+  & th {
+    border: 1px solid #ccd;
+    padding: 0.5rem 1rem;
+    background-color: #ccc;
+  }
   & td {
     padding: 0.5rem;
     margin: 0;
+    vertical-align: top;
+    border: 1px solid #ddd;
   }
   & tr {
     border: 1px solid #ccd;
