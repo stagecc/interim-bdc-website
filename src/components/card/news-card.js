e9001c012710fdf09119485ec7c80438e605639f
Matt Watson <mwatson@renci.org>
2024-08-01 12:47:56 -0400

    fix transition and focus link/card mismatch

--- src/components/card/news-card.js
+++ src/components/card/news-card.js
@@ -18,16 +18,17 @@ const NewsCardContainer = styled(Card)`
   justify-content: space-between;
   padding: 1rem;
   filter: 
-    drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.05))
-    drop-shadow(4px -4px 4px rgba(0, 0, 0, 0.05))
-    drop-shadow(-4px 8px 4px rgba(0, 0, 0, 0.05));
+    drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.1))
+    drop-shadow(4px -4px 4px rgba(0, 0, 0, 0.1))
+    drop-shadow(-4px 8px 4px rgba(0, 0, 0, 0.1));
   &:hover, &:focus {
-  filter: 
-    drop-shadow(4px 4px 6px rgba(0, 0, 0, 0.1))
-    drop-shadow(6px -6px 6px rgba(0, 0, 0, 0.1))
-    drop-shadow(-6px 8px 6px rgba(0, 0, 0, 0.1));
+    filter: 
+      drop-shadow(4px 4px 6px rgba(0, 0, 0, 0.15))
+      drop-shadow(6px -6px 6px rgba(0, 0, 0, 0.2))
+      drop-shadow(-6px 8px 6px rgba(0, 0, 0, 0.15))
+      brightness(1.1);
   };
-  transition: filter 100ms ease-in;
+  transition: filter 250ms ease-in;
 `
 
 const NewsCardHeading = styled.h2`
@@ -55,50 +56,48 @@ export const NewsCard = ({ newsItem }) => {
   } = newsItem;
 
   return (
-    <Link to={newsLink} noIcon>
-      <NewsCardContainer>
-        <div>
-          <NewsCardHeading>
-            {newsTitle}
-          </NewsCardHeading>
-          <Paragraph style={{letterSpacing: "1px", color: '#fff', fontSize: '1rem'}}>
-            {newsDate}
-          </Paragraph>
-        </div>
+    <NewsCardContainer as={ Link } to={newsLink} noIcon>
+      <div>
+        <NewsCardHeading>
+          {newsTitle}
+        </NewsCardHeading>
+        <Paragraph style={{letterSpacing: "1px", color: '#fff', fontSize: '1rem'}}>
+          {newsDate}
+        </Paragraph>
+      </div>
 
-        <div style={{display: "block"}}>
-          <Paragraph style={{letterSpacing: "1px", color: '#fff'}}>
-            {newsSource}
-          </Paragraph>
-          <div style={{display:'flex', flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'flex-end'}}>
-            {
-              loginRequired && (
-              <Paragraph style={{
-                letterSpacing: "0.5px", 
-                color: '#fff', 
-                fontSize: '0.7rem',
-                fontStyle: 'italic',
-                marginRight: '1rem',
-                marginBottom: 0}} >
-                *This article is located behind a paywall or site that requires log in.
-              </Paragraph>
-              )
-            }
-            { external &&
-              (
-                <div style={{textAlign: "right"}}>
-                  <ExternalLinkIcon
-                    fill={"#fff"}
-                    size={14}
-                    style={{ marginLeft: "0.25rem" }}
-                  />
-                </div>
-              )
-            }
+      <div style={{display: "block"}}>
+        <Paragraph style={{letterSpacing: "1px", color: '#fff'}}>
+          {newsSource}
+        </Paragraph>
+        <div style={{display:'flex', flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'flex-end'}}>
+          {
+            loginRequired && (
+            <Paragraph style={{
+              letterSpacing: "0.5px", 
+              color: '#fff', 
+              fontSize: '0.7rem',
+              fontStyle: 'italic',
+              marginRight: '1rem',
+              marginBottom: 0}} >
+              *This article is located behind a paywall or site that requires log in.
+            </Paragraph>
+            )
+          }
+          { external &&
+            (
+              <div style={{textAlign: "right"}}>
+                <ExternalLinkIcon
+                  fill={"#fff"}
+                  size={14}
+                  style={{ marginLeft: "0.25rem" }}
+                />
+              </div>
+            )
+          }
 
-          </div>
         </div>
-      </NewsCardContainer>
-    </Link>
+      </div>
+    </NewsCardContainer>
   );
 };
