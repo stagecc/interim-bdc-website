ab3c7ae53e84d4d7aed03d26cc9bf434ef6656e0
Matt Watson <mwatson@renci.org>
2024-08-02 12:50:47 -0400

    remove padding on floating yt video
    add noPadding prop to FloatingContentWrapper

--- src/components/layout/floating-content.js
+++ src/components/layout/floating-content.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
 import { Card } from "../../components/card";
 
-export const FloatingContentWrapper = styled(Card)(({ placement, type })=>`
+export const FloatingContentWrapper = styled(Card)(({ placement, type, noPadding = false })=>`
   width: 45%;
   float: ${ placement === "left" ? "left" : "right" };
   background-color: ${ type === "text" && "rgba(237, 240, 244, 0.8)" };
   box-shadow: ${ type === "text" && "4px 4px 12px rgba(186, 194, 204, 0.5)" };
-  padding: 0 1.5rem;
+  padding: ${ noPadding ? 0 : '0 1.5rem' };
   margin-top: 1rem;
   margin-bottom: 1rem;
   margin-left: ${ placement === "right" ? "1.5rem" : 0 };
