94b436406dfec120fa18744ac1c4e549082263f1
Matt Watson <mwatson@renci.org>
2024-08-09 00:48:26 -0400

    move markdown into dir structure
    del
    
    fix imports

--- src/components/markdown/index.js
+++ src/components/markdown/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { MDXProvider } from "@mdx-js/react"
+import React from "react";
+import { MDXProvider } from "@mdx-js/react";
 import {
   Heading,
   Paragraph,
   Subheading,
   Title,
-} from './typography'
-import { Link } from "./link"
+} from "../typography";
+import { Link } from "../link";
 
 export const componentMap = () => ({
   p: function paragraph({ node, children, ...props }) {
