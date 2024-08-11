4c50c6a02bd2dbbe67c3ce1e9291f092b4c06225
Matt Watson <mwatson@renci.org>
2024-08-11 13:19:52 -0400

    define container & menu prop types, use seo
    define container & menu prop types, use seo

--- src/components/layout/container.js
+++ src/components/layout/container.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
+import PropTypes from 'prop-types';
 import styled from "styled-components";
 import { Container as Grid, Row, Col, Visible } from "react-grid-system";
 import { Seo } from "../seo";
 import { Title } from "../typography";
-import { PageNavMenu } from "./page-nav-menu";
+import { PageNavMenu, PageNavMenuPropTypes } from "./page-nav-menu";
 
 export const Container = styled.div`
-  // & * { border: 1px solid #f99; }
   width: ${props => props.width || "100%"};
   max-width: ${props => props.maxWidth || "100%"};
   ${props => (props.minWidth ? `max-width: ${props.minWidth};` : undefined)}
@@ -21,34 +21,54 @@ export const PageContainer = styled(Container)`
   margin-bottom: ${props => (props.gutters ? BOTTOM_GUTTER : 0)};
 `;
 
-export const PageContent = ({title, hideTitle, menuItems, children, ...props}) => {
+export const PageContent = ({
+  title,
+  description = "",
+  keywords = "",
+  hideTitle = false,
+  menuItems = undefined,
+  children,
+  ...props
+}) => {
   return (
     <PageContainer {...props}>
-      { title && <Seo title={title} description="" keywords="" /> }
-      
-      { menuItems ? (
-        <Grid fluid>
-          <Row>
-            <Visible lg xl xxl>
-              <Col lg={3}>
-                <PageNavMenu menuItems={menuItems}/>
+      { title && <Seo
+        title={title}
+        description={description}
+        keywords={keywords}
+      /> }
+      {
+        menuItems ? (
+          <Grid fluid>
+            <Row>
+              <Visible lg xl xxl>
+                <Col lg={3}>
+                  <PageNavMenu menuItems={menuItems}/>
+                </Col>
+              </Visible>
+              <Col xs={12} lg={9} style={{marginBottom: '8rem'}}>
+                { (title && !hideTitle) && <Title>{title}</Title> }
+                {children}
               </Col>
-            </Visible>
-            <Col xs={12} lg={9} style={{marginBottom: '8rem'}}>
-              { (title && !hideTitle) && <Title>{title}</Title> }
-              {children}
-            </Col>
-          </Row>
-        </Grid>
-      ) : (
-        <Fragment>
-          { (title && !hideTitle) && <Title>{title}</Title> }
-
-          {children}
-        </Fragment>
-      )
+            </Row>
+          </Grid>
+        ) : (
+          <Fragment>
+            { (title && !hideTitle) && <Title>{title}</Title> }
+            {children}
+          </Fragment>
+        )
       }
-      
     </PageContainer>
   )
 }
+
+
+PageContent.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string,
+  keywords: PropTypes.string,
+  hideTitle: PropTypes.bool,
+  menuItems: PropTypes.arrayOf(PageNavMenuPropTypes),
+  children: PropTypes.node,
+};
