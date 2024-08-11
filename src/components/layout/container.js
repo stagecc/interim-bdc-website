9e465999c10b5791a8987f8fd739d239f7789a7f
Matt Watson <mwatson@renci.org>
2024-08-11 13:15:14 -0400

    remove unused components

--- src/components/layout/container.js
+++ src/components/layout/container.js
@@ -21,42 +21,7 @@ export const PageContainer = styled(Container)`
   margin-bottom: ${props => (props.gutters ? BOTTOM_GUTTER : 0)};
 `;
 
-export const BandedContainer = styled(Container)`
-  position: relative;
-  &::before {
-    position: absolute;
-    content: "";
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: calc(100% - 6rem);
-    transform: translateY(3rem);
-    background-color: #8997b4aa;
-  }
-`;
-
-export const BackgroundImageContainer = styled(Container)`
-  position: relative;
-  &::before {
-    position: absolute;
-    content: "";
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: var(--color-eggplant);
-    background-image: url(${props => props.image});
-    background-position: center;
-    background-size: cover;
-    background-blend-mode: multiply;
-    filter: brightness(2.5);
-    z-index: -1;
-    clip-path: polygon(0% 5rem, 50% 8rem, 100% 5rem, 100% 100%, 0% 100%);
-  }
-`;
-
 export const PageContent = ({title, hideTitle, menuItems, children, ...props}) => {
-
   return (
     <PageContainer {...props}>
       { title && <Seo title={title} description="" keywords="" /> }
