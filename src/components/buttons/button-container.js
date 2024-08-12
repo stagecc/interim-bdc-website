e24a40dfa018521e1a4fd165a32831e68c89af01
suejinkim20 <kim.suejin.20@gmail.com>
2024-08-12 11:42:36 -0400

    add responsive flex direction on button container

--- src/components/buttons/button-container.js
+++ src/components/buttons/button-container.js
@@ -1,10 +1,23 @@
+import React from 'react'
 import styled from "styled-components";
+import { useWindowWidth } from "../../hooks";
 
-export const ButtonContainer = styled.div`
+const Wrapper =  styled.div(({ compact }) => `
   display: flex;
+  flex-direction: ${compact ? 'column' : 'row'};
   justify-content: center;
   text-align: center;
   padding: 2rem 0;
   gap: 1.5rem;
   margin: 0 1rem;
-`
+`);
+
+export const ButtonContainer = ({children}) => {
+  const { isCompact } = useWindowWidth();
+
+  return (
+    <Wrapper compact={isCompact}>
+      {children}
+    </Wrapper>
+  )
+}
