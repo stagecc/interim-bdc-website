12cc8b133147e1fedb573c5f3cc441526517b6e5
suejinkim20 <kim.suejin.20@gmail.com>
2024-08-16 16:23:16 -0400

    add hash functionality to RouteChangeScroller useEffect

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -60,11 +60,19 @@ const LayoutWrapper = styled.div(
 // styles/customize.css defines smooth scroll behavior site-wide.
 // however, we want to snap to the top of page on a route change.
 const RouteChangeScroller = () => {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
   
   useEffect(() => {
-    window.scrollTo({ top: 0, behavior: 'instant' });
-  }, [pathname]);
+
+    if (hash) {
+      const target = document.getElementById(hash.replace('#', ''))
+      if (target) {
+        target.scrollIntoView({ behavior: 'smooth' })
+      }
+    } else {
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    }
+  }, [pathname, hash]);
 
   return null;
 };
