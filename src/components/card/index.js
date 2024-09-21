0cb5b07e1bac45087e35500185c5cf8b46cc1632
Matt Watson <mwatson@renci.org>
2024-09-20 22:25:13 -0400

    create error card

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,5 +2,7 @@ export * from "./card";
 export * from "./card-header";
 export * from "./card-body";
 export * from "./card-footer";
+
+export * from "./error-card";
 export * from "./news-card";
 export * from "./next-steps-card";
