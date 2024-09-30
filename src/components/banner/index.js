07e72d27eed5cc3b4aa6e4af7088def794db84ac
Matt Watson <mwatson@renci.org>
2024-09-30 09:58:46 -0400

    copy in prev site banner

---
+++ src/components/banner/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+// import { useLocalStorage } from "../../hooks"
+import { IconButton } from "../buttons";
+import { CloseIcon } from "../icons";
+
+const Wrapper = styled.div(({ active }) => `
+  position: relative;
+  border: solid var(--color-crimson);
+  background-color: rgb(251, 232, 227);
+  color: var(--color-crimson);
+  border-width: 0 0 0.75rem 0;
+  transition: border-color 250ms;
+  & .toggler {
+    transform: translate3d(0, ${ active ? '5px' : 0 }, 0);
+    width: 40px;
+    height: 50px;
+    clip-path: polygon(0% 0%, 100% 0%, 100% calc(100% - 15px), 50% 100%, 0% calc(100% - 15px));
+    z-index: 999;
+    background-color: var(--color-crimson);
+    position: absolute;
+    right: 1rem;
+    top: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    transition: transform 250ms ease-out 250ms;
+    &:hover {
+      transform: translate3d(0, ${ active ? '0px' : '10px' }, 0);
+    }
+  }
+  & svg {
+    opacity: 0.75;
+    transition: fill 250ms, opacity 250ms;
+  }
+  &:hover, &:focus-within {
+    border-color: var(--color-crimson);
+    & .toggler {
+      transition: background-color 250ms, transform 50ms ease-out;
+      background-color: var(--color-crimson);
+      filter: brightness(1.0);
+      & svg {
+        opacity: 1.0;
+      }
+    }
+  }
+`);
+
+export const Banner = ({ children, openedIcon, closedIcon }) => {
+  const [open, setOpen] = useState(true);
+  
+  return (
+    <Wrapper active={ open }>
+      { open && children }
+      <IconButton onClick={ () => setOpen(!open) } className="toggler">
+        { open ? openedIcon : closedIcon }
+      </IconButton>
+    </Wrapper>
+  );
+
+  return null;
+};
