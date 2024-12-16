4b2ca6019cc64aae25c0a1654ea224ae69c6d594
suejinkim20 <kim.suejin.20@gmail.com>
2024-12-16 09:29:56 -0500

    stack headshot and bio vertically on smaller screens

--- src/components/eep-bios/eep-headshot.js
+++ src/components/eep-bios/eep-headshot.js
@@ -1,8 +1,8 @@
+import React from "react";
 import styled from "styled-components";
 import { GatsbyImage } from "gatsby-plugin-image"
 
-
-export const EepHeadshot = styled(GatsbyImage)`
+const HeadshotImage = styled(GatsbyImage)`
   max-width: 200px;
   max-height: 200px;
   min-width: 200px;
@@ -11,3 +11,12 @@ export const EepHeadshot = styled(GatsbyImage)`
   margin-right: 1rem;
   margin-top: 1.75rem;
 `;
+
+export const EepHeadshot = ({image}) => {
+
+  return (
+    <div style={{display: 'flex', justifyContent: 'center'}}>
+      <HeadshotImage image={image}/>
+    </div>
+  )
+}
--- src/components/eep-bios/eep-member.js
+++ src/components/eep-bios/eep-member.js
@@ -3,14 +3,15 @@ import { getImage } from "gatsby-plugin-image";
 import { graphql, useStaticQuery } from 'gatsby';
 import styled from "styled-components";
 import { EepHeadshot } from './';
+import { useWindowWidth } from "../../hooks";
 
-const FlexWrapper = styled.div`
+const FlexWrapper = styled.div(({ compact }) => `
   display: flex;
-  flex-direction: row;
+  flex-direction: ${compact ? 'column' : 'row'};
   h3 {
     margin-bottom: 0;
-  }
-`
+  };
+`);
 
 export const EepMember = ({imageName, children}) => {
   const data = useStaticQuery(graphql`
@@ -27,13 +28,14 @@ export const EepMember = ({imageName, children}) => {
       }
     }
   `);
+  const { isCompact } = useWindowWidth();
 
 
   const imageNode = data.allFile.nodes.find(node => node.name === imageName);
   const image = getImage(imageNode);
 
   return (
-    <FlexWrapper>
+    <FlexWrapper compact={isCompact}>
       <EepHeadshot image={image}/>
       <div>
         {children}
