9a1554a787437812a7eccabaec61ac96ffb90273
suejinkim20 <kim.suejin.20@gmail.com>
2025-06-16 17:32:28 -0400

    adjust ButtonContainer to apply equal widths to multiple child button links

--- src/components/buttons/button-container.js
+++ src/components/buttons/button-container.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React from 'react';
 import styled from "styled-components";
 import { useWindowWidth } from "../../hooks";
 
-const Wrapper =  styled.div(({ compact }) => `
+const Wrapper = styled.div(({ compact, width }) => `
   display: flex;
   flex-direction: ${compact ? 'column' : 'row'};
   justify-content: center;
@@ -10,14 +10,19 @@ const Wrapper =  styled.div(({ compact }) => `
   padding: 2rem 0;
   gap: 1.5rem;
   margin: 0 1rem;
+  & a {
+    width: ${width};
+  };
 `);
 
-export const ButtonContainer = ({children}) => {
+export const ButtonContainer = ({ children }) => {
   const { isCompact } = useWindowWidth();
+  const count = React.Children.count(children);
+  const width = count > 1 ? `${100 / count}%` : 'auto';
 
   return (
-    <Wrapper compact={isCompact}>
+    <Wrapper compact={isCompact} width={width}>
       {children}
     </Wrapper>
-  )
-}
+  );
+};
