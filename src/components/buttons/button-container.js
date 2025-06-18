a3523b7468e74c79ffa6f545d15bcef62f82c2b6
suejinkim20 <kim.suejin.20@gmail.com>
2025-06-18 14:19:47 -0400

    apply flex layout to ButtonContainer and handle single-button case

--- src/components/buttons/button-container.js
+++ src/components/buttons/button-container.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import styled from "styled-components";
 import { useWindowWidth } from "../../hooks";
+import { Button } from './button';
 
-const Wrapper = styled.div(({ compact, width }) => `
+const Wrapper = styled.div(({ compact }) => `
   display: flex;
   flex-direction: ${compact ? 'column' : 'row'};
   justify-content: center;
   text-align: center;
   padding: 2rem 0;
   gap: 1.5rem;
-  margin: 0 1rem;
-  & a {
-    width: ${width};
-  };
+  margin: 0 auto;
+  /* make each Button take equal width in row layout */
+  ${Button} {
+    flex: 1;
+  }
+  /* if there's only one Button, shrink container to fit it */
+  &:has(${Button}:only-of-type){
+    width: max-content;
+  }
 `);
 
 export const ButtonContainer = ({ children }) => {
   const { isCompact } = useWindowWidth();
-  const count = React.Children.count(children);
-  const width = count > 1 ? `${100 / count}%` : 'auto';
 
   return (
-    <Wrapper compact={isCompact} width={width}>
+    <Wrapper compact={isCompact}>
       {children}
     </Wrapper>
   );
