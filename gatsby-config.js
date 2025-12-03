77e6f4e28b01b059bf472cd0f75b6d79e9a2104d
Matt Watson <mwatson@renci.org>
2025-12-03 12:53:10 -0500

    Integrate gatsby-plugin-csp for stricter Content-Security-Policy
    Integrates gatsby-plugin-csp to enhance the Content-Security-Policy (CSP) by removing 'unsafe-inline' directives from script-src and style-src. This significantly reduces XSS vulnerabilities. The plugin automatically generates hashes for inline scripts and styles, ensuring functionality while improving security.

--- gatsby-config.js
+++ gatsby-config.js
@@ -184,5 +184,24 @@ module.exports = {
         trackingIds: ["G-2M4JYYSBD3"],
       },
     },
+    {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false,
+        mergeScriptHashes: true, // you should always include script hashes
+        mergeStyleHashes: true, // you should always include style hashes
+        directives: {
+          "script-src": `'self' https://www.googletagmanager.com https://widget.freshworks.com https://www.youtube.com`,
+          "style-src": `'self' https://fonts.googleapis.com https://widget.freshworks.com`,
+          "img-src": `'self' data: https://widget.freshworks.com`,
+          "font-src": `'self' https://fonts.gstatic.com https://widget.freshworks.com`,
+          "connect-src": `'self' https://www.google-analytics.com https://widget.freshworks.com https://search.biodatacatalyst.renci.org https://epxuifil2cc4sqfqws62zejcwi0cgfds.lambda-url.us-east-1.on.aws`,
+          "object-src": `'none'`,
+          "base-uri": `'self'`,
+          "frame-ancestors": `'none'`,
+        },
+      },
+    },
   ]
 };
